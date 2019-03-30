4d84e05baa49185960eabf2c0380186f21d7c8a4
Jean <tech7dev@gmail.com>
2019-03-30 09:05:45 +0200

    Initial commit

--- app/src/main/java/cd/mercipro/smallshop/Activities/MyProductActivity.java
+++ app/src/main/java/cd/mercipro/smallshop/Activities/MyProductActivity.java
@@ -2,14 +2,29 @@ package cd.mercipro.smallshop.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import cd.mercipro.smallshop.R;
 
 public class MyProductActivity extends AppCompatActivity {
 
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+    private EditText txtNom_Prod, txtPU_achat, txtPU_vente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myproduct);
+
+        //init view
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        txtNom_Prod = findViewById(R.id.txtNom_Prod);
+        txtPU_achat = findViewById(R.id.txtPU_achat);
+        txtPU_vente = findViewById(R.id.txtPU_vente);
     }
 }
